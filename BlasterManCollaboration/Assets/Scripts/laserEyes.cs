5fc835b288acaf7a06146d47e02b38d7b0eb7874
mike2782 <mikehavardbeatbox@hotmail.co.uk>
2021-12-18 19:46:01 +0000

    Added a raycast to laser eyes (not working currently)
    I have added a raycast to the laser eyes and changed a few other bits. The laser eyes raycast is not working properly yet. I think I have an idea for a work around if I can't get it working tomorrow

--- BlasterManCollaboration/Assets/Scripts/laserEyes.cs
+++ BlasterManCollaboration/Assets/Scripts/laserEyes.cs
@@ -18,26 +18,22 @@ public class laserEyes : MonoBehaviour
     //This was used for testing
     float length = 0.0f;
 
-
     // Start is called before the first frame update
-
     void Start()
     {
         //We take the children of the parent object (prefab object) and put the lines into the array
         lines = gameObject.GetComponentsInChildren<LineRenderer>();
     }
 
-
-
     // Update is called once per frame
     void Update()
     {
         //The length of the line increased with the frames
         length += Time.deltaTime * speed;
-        Debug.Log("Length: " + length);
+        //Debug.Log("Length: " + length);
 
         //Loop over each line, sets the position
-        foreach(LineRenderer line in lines)
+        foreach (LineRenderer line in lines)
         {
             line.SetPosition(1, new Vector3(0.0f, length, 0.0f));
         }
@@ -46,10 +42,22 @@ public class laserEyes : MonoBehaviour
         deleteTimer -= Time.deltaTime;
 
         //Statement to remove the lines from the scene after the timer reaches 0. Timer then resets
-        if (deleteTimer <= 0) {
-            Debug.Log("delete lines");
+        if (deleteTimer <= 0)
+        {
+            //Debug.Log("delete lines");
             deleteTimer = 0.1f;
             Destroy(this.gameObject);
+
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
+            Debug.DrawLine(transform.position, hit.point);
+            if (hit && hit.collider.name != "Player")
+            {
+                Debug.Log("Hit:" + hit.collider.name);
+            }
             
         }
     }
