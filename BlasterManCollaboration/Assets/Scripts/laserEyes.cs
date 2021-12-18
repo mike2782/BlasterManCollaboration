12f495f360fa27746225f530d1a454bc856d8b94
mike2782 <mikehavardbeatbox@hotmail.co.uk>
2021-12-18 17:12:45 +0000

    File tidy up
    Deleted some un-needed scripts and changed a few bits

--- BlasterManCollaboration/Assets/Scripts/laserEyes.cs
+++ BlasterManCollaboration/Assets/Scripts/laserEyes.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class laserEyes : MonoBehaviour
 {
+    //Set the speed of the line drawing movement
     [SerializeField]
     private int speed = 100;
 
+    //A timer to delete the line
     [SerializeField]
     private float deleteTimer = 0.1f;
 
-
+    //Setting an array of lines in the line renderer
     private LineRenderer[] lines;
+
+    //This was used for testing
     float length = 0.0f;
+
+
     // Start is called before the first frame update
 
     void Start()
     {
+        //We take the children of the parent object (prefab object) and put the lines into the array
         lines = gameObject.GetComponentsInChildren<LineRenderer>();
     }
 
@@ -25,16 +32,20 @@ public class laserEyes : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //The length of the line increased with the frames
         length += Time.deltaTime * speed;
         Debug.Log("Length: " + length);
+
+        //Loop over each line, sets the position
         foreach(LineRenderer line in lines)
         {
             line.SetPosition(1, new Vector3(0.0f, length, 0.0f));
         }
 
-        Debug.Log(deleteTimer);
+        //Timer counts down
         deleteTimer -= Time.deltaTime;
 
+        //Statement to remove the lines from the scene after the timer reaches 0. Timer then resets
         if (deleteTimer <= 0) {
             Debug.Log("delete lines");
             deleteTimer = 0.1f;
